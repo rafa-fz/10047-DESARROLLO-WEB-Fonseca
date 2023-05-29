2beffd063114a31327ac056ecbe3c53054f9a6d3
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2023-05-28 22:34:01 -0500

    Datos_Primitivos_ej

--- Tipos de Datos Primitivos/Ejercicio_1.js
+++ Tipos de Datos Primitivos/Ejercicio_1.js
@@ -1,11 +1,11 @@
+// Ejercicio 1: En este ejercicio, se solicita al usuario que ingrese un número como cadena de texto, se
+// convierte a tipo de dato numérico y se realiza una operación matemática.
+
 
 let numeroComoCadena = prompt("Ingresa un número como cadena de texto:");
 
-
 let numero = Number(numeroComoCadena);
-
 
 let resultado = numero * 2
 
-
-console.log(`El resultado de multiplicar {numero} por 2 es: ${resultado}`);
+console.log(`El resultado de multiplicar ${numero} por 2 es: ${resultado}`);
---
+++ Tipos de Datos Primitivos/Ejercicio_2.js
@@ -0,0 +1,6 @@
+// Ejercicio 2: En este ejercicio, se solicita al usuario que ingrese un valor y se muestra en la consola el
+// tipo de dato utilizando el operador typeof.
+
+let valor = prompt("Ingresa un valor:");
+
+console.log(`El tipo de dato de ${valor} es: ${typeof valor}`);
