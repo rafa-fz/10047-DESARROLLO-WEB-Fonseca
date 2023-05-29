2a2822e403815222347bd7d95fd2ebd56e277ecd
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2023-05-29 18:35:57 -0500

    Create Ejercicios.js

---
+++ Test de JavaScript/1.2 Variables y Operaciones/Ejercicios.js
@@ -0,0 +1,19 @@
+// 1.2.3 Traduce a código JavaScript las variables del ejemplo anterior.
+
+let nombre = "Juan"
+let apellido = "Pérez"
+let userESPE = "jperez"
+let edad = 25
+let email = "jperez@espe.edu.ec"
+let mayorEdad = true
+let dineroAhorrado = 25000
+let deudas = 15000
+
+// 1.2.4 Calcula e imprime las siguientes variables a partir de las variables del ejemplo anterior:
+// • Nombre completo (nombre y apellido)
+let nombreCompleto = nombre + " " + apellido;
+console.log(`El nombre completo es: ${nombreCompleto}`);
+
+// • Dinero real (dinero ahorrado menos deudas)
+let dineroReal = dineroAhorrado - deudas;
+console.log(`El dinero real es: ${dineroReal}`);
