0ccde4e1255383e7544df8f44c1687cfb5d9b1da
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2023-05-28 22:38:23 -0500

    variables

---
+++ Variables, Declaración, Inicialización, console.log/ejercicio_1.js
@@ -0,0 +1,9 @@
+// Ejercicio 1: En este ejercicio, se declaran dos variables, se les asignan valores y se realiza una
+// operación matemática con ellas. El resultado se muestra en la consola.
+
+let numero1 = 10;
+let numero2 = 20;
+
+let resultado = numero1 + numero2;
+
+console.log(`El resultado de sumar ${numero1} y ${numero2} es: ${resultado}`);
---
+++ Variables, Declaración, Inicialización, console.log/ejercicio_2.js
@@ -0,0 +1,9 @@
+// Ejercicio 2: En este ejercicio, se declaran variables con valores de cadena de texto y se realiza la
+// concatenación de ellas con texto adicional. El resultado se muestra en la consola.
+
+let nombre = "Juan";
+let apellido = "Perez";
+
+let nombreCompleto = nombre + " " + apellido;
+
+console.log(`El nombre completo es: ${nombreCompleto}`);
