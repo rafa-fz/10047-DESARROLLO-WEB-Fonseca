dad81b3fcefbfe376c4f308fcd89f10da6164052
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2023-05-28 22:34:16 -0500

    typeof_ej

---
+++ TypeOf/Ejercicio_1.js
@@ -0,0 +1,6 @@
+// Ejercicio 1: En este ejercicio, se declara una variable con un valor y se muestra en la consola el tipo de
+// dato utilizando el operador typeof.
+
+let valor = 10;
+
+console.log(`El tipo de dato de ${valor} es: ${typeof valor}`);
---
+++ TypeOf/Ejercicio_2.js
@@ -0,0 +1,8 @@
+// Ejercicio 2: En este ejercicio, se define una función que recibe un argumento y se muestra en la consola
+// el tipo de dato utilizando el operador typeof.
+
+function tipoDeDato(valor) {
+    console.log(`El tipo de dato de ${valor} es: ${typeof valor}`);
+}
+
+tipoDeDato(true);
