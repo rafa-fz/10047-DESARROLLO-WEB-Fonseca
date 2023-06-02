ba76cdd3e4056ad3e24b00aae912bcf270e8e58a
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2023-06-02 18:22:22 -0500

    Update Parte Practica-Fonseca Rafael.js

--- Evaluación Formativa Primer Parcial/Parte Practica-Fonseca Rafael.js
+++ Evaluación Formativa Primer Parcial/Parte Practica-Fonseca Rafael.js
@@ -32,7 +32,30 @@ function imprimirArray(array) {
     }
 }
 
-// 6.	¿Cuánto es 2 + 2? Respuesta: 4
+// 6.	codigo deL: ¿Cuánto es 2 + 2? Respuesta: 4
+
+let a = 2;
+let b = 2;
+
+console.log("Cuanto es " + a + " + " + b + "?");
+console.log("La respuesta es: " + suma(a, b));
+
+// 7.	¿Qué es un array?
+// 2.	Un array es un tipo de dato en JavaScript que permite almacenar varios valores en una sola variable.
+
+
+// 8.	¿Qué es un objeto?
+// 2.	Un objeto es un tipo de dato en JavaScript que permite almacenar pares clave-valor.
+
+
+// 9.	¿Cuándo es mejor usar objetos o arrays?
+// 3.	Es mejor usar objetos cuando se necesitan almacenar pares clave-valor.
+
+
+// 10.	¿Puedo mezclar arrays con objetos o incluso objetos con arrays?
+// 2.	Sí, es posible mezclar arrays con objetos y objetos con arrays en JavaScript.
+
+
 
 
 // 11.	Crea una función llamada "primerElementoArray" que reciba como parámetro un array y retorne el primer elemento.
@@ -48,3 +71,39 @@ function imprimirElementosArray(array) {
         console.log(array[i]);
     }
 }
+
+// 13.	¿Cuál es la salida del siguiente código?
+// let nombre = "Juan";
+
+// function saludar() {
+//   let nombre = "María";
+//   console.log("Hola " + nombre);
+// }
+
+// saludar();
+// console.log("Hola " + nombre);
+
+// LA SALIDA es:
+// Hola María
+// Hola Juan
+
+// •	¿Cuál es la salida del siguiente código?
+// let x = 5;
+
+// function sumarUno() {
+//   x++;
+// }
+
+// function restarUno() {
+//   x--;
+// }
+
+// sumarUno();
+// sumarUno();
+// restarUno();
+// console.log(x);
+
+// La salida del siguiente código sería: 6
+
+
+
